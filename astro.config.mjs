07acf49fe433f7d7fa4a0aa71440ecd481119a37
Korbs <korbs@sudovanilla.com>
2023-09-23 22:30:51 -0400

    Add docs

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'TeaClient Documentations',
+			lastUpdated: true,
+			editLink: {
+			  baseUrl: 'https://github.com/TeaClientMC/Docs/edit/starlight/',
+			},
+			social: {
+				github: 'https://github.com/TeaClientMC/Docs',
+				discord: 'https://discord.gg/aKXRP4WC7P',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'Intro', link: '/getting-started/intro/' },
+						{ label: 'Installation', link: '/getting-started/install/', badge: { text: 'Experimental', variant: 'caution' }, },
+					],
+				},
+				{
+					label: 'API',
+					items: [
+						{ label: 'Intro', link: '/api/intro/' },
+						{ label: 'Tea+', link: '/api/tea/', badge: 'New' },
+					],
+				},
+			],
+		}),
+	],
+})
